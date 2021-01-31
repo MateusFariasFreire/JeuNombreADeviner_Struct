9b05f31ab6e2847e42224a393d6490484f6059b3
MateusFariasFreire <mateus.fariasfreire@limayrac.fr>
2021-01-31 21:45:36 +0100

    Headers avec les commentaires mis à jour

--- include/JeuNombreADeviner.h
+++ include/JeuNombreADeviner.h
@@ -25,16 +25,13 @@ int nbTentatives;
 
 
 
-
-
 // Nom :InitJoueur
 // Rôle : Crée un joueur. Initialise toutes les informations du joueur.
 //        Le nombre de tentatives, de parties gagnées et de parties jouées seront à 0.
-// Paramètres d'entrée :
-// Paramètres de sortie :
-// Paramètres d'entrée/sortie :
+// Paramètres d'entrée : nom du joueur à créer
+// Paramètres d'entrée/sortie : joueur à créer
 
-void InitJoueur (TJoueur &joueurAcreer, string un_nom);
+void InitJoueur (TJoueur & joueurAcreer, string un_nom);
 
 
 // Nom :TirerNombreMystere
@@ -46,31 +43,28 @@ int TirerNombreMystere();
 // Nom :JouerPartie
 // Rôle : Fait jouer une partie au joueur passé en paramètre
 //        A la fin, met à jour les informations du joueur
-// Paramètres d'entrée:
-// Paramètres de sortie:
-// Paramètres d'entrée/sortie :
+// Paramètres d'entrée: nombre à deviner
+// Paramètres d'entrée/sortie : joueur
 
-void JouerPartie (TJoueur &un_joueur, int nombreADeviner);
+void JouerPartie (TJoueur & un_joueur, int nombreADeviner);
 
 // Nom : MajResultatsJoueur
 // Rôle : met à jour les informations du joueur passé en paramètre
-// Paramètres d'entrée:
-// Paramètres de sortie:
-// Paramètres d'entrée/sortie :
+// Paramètres d'entrée: joueur / nombre d'essais / victoire ou non du joueur
+// Paramètres d'entrée/sortie : joueur
 
-void MajResultatsJoueur(TJoueur joueur, int nbEssais, bool gagne);
-
+void MajResultatsJoueur(TJoueur & joueur, int nbEssais, bool gagne);
 
 
 // Nom : ResultatsJoueur
 // Rôle : indique les résultats du joueur passé en paramètre
 //        le nombre de parties gagnées, le nombre de parties perdues, le nombre d'essais total
 //        La fonction N'affiche PAS les informations à l'écran
-// Paramètres d'entrée:
-// Paramètres de sortie:
-// Paramètres d'entrée/sortie :
+// Paramètres d'entrée: joueur
+// Paramètres de sortie: aucun
+// Paramètres d'entrée/sortie : nbsucces / nbechec / nbessais
 
-void ResultatsJoueur(TJoueur joueur, int &nbsucces, int &nbechec, int & nbessais);
+void ResultatsJoueur(TJoueur joueur, int & nbsucces, int & nbechec, int & nbessais);
 
 
 // Nom :Nom
